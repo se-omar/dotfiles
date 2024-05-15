5a6754a26082cf771d395b7031afd70885de55da
Omar Ayman <se.omarayman@gmail.com>
2024-05-15 17:31:16 +0300

    remove useless command from tmux-run-backend

--- tmux-scripts/run-swypex-backend.sh
+++ tmux-scripts/run-swypex-backend.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Create a new window
-tmux new-window -t backend:2 -c '~/work/source/backend'
+tmux new-window -t backend:2
 
 # Split the window into two panes vertically
-tmux split-window -v
 tmux split-window -v
 tmux select-layout even-horizontal
 
 # Run different commands in each pane
-tmux send-keys -t backend:2.0 './gradlew dev ; exit' Enter
-tmux send-keys -t backend:2.1 './gradlew api:bootRun' Enter
-tmux send-keys -t backend:2.2 './gradlew graphql-api:bootRun' Enter
+tmux send-keys -t backend:2.0 './gradlew api:bootRun' Enter
+tmux send-keys -t backend:2.1 './gradlew graphql-api:bootRun' Enter
 
 tmux select-window -t backend:1
 tmux send-keys -t backend:1 'vi .' Enter
