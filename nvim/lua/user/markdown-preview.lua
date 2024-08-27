ce1265a9cd2e14b95b3fc83f29322f560e88b748
Omar Ayman <se.omarayman@gmail.com>
2024-08-27 03:22:03 +0300

    add mardownPreview

---
+++ nvim/lua/user/markdown-preview.lua
@@ -0,0 +1,11 @@
+local M = {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = "cd app && npm install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+}
+
+return M
