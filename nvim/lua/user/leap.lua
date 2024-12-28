6cfe1e98d6f75651e01a45f9555410f2f367c2fb
Omar Ayman <se.omarayman@gmail.com>
2024-12-28 14:02:36 +0200

    fix leap cursor

--- nvim/lua/user/leap.lua
+++ nvim/lua/user/leap.lua
@@ -6,15 +6,6 @@ local M = {
 function M.config()
 	require('leap').add_default_mappings()
 	vim.api.nvim_set_hl(0, 'LeapLabelPrimary', {   fg='Black', bg='Cyan' })
-	-- Hide the (real) cursor when leaping, and restore it afterwards.
-	vim.api.nvim_create_autocmd('User', {
-		pattern = 'LeapEnter',
-		callback = function()
-			vim.cmd.hi('Cursor', 'blend=100')
-			vim.opt.guicursor:append { 'a:Cursor/lCursor' }
-		end,
-	}
-	)
 	vim.api.nvim_create_autocmd('User', {
 		pattern = 'LeapLeave',
 		callback = function()
