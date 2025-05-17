afbd0146c7adee5e7345695076cc285ab7d38f61
Omar Ayman <se.omarayman@gmail.com>
2025-05-17 15:26:39 +0300

    finish modularizing plugins

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('github-theme').setup {
+      options = {
+        styles = {
+          comments = 'italic',
+        },
+      },
+    }
+
+    vim.cmd 'colorscheme github_dark_dimmed'
+  end,
+}
