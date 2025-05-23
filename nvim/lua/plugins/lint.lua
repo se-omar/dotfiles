5eaa4caa7660e7f11f4734626ef12067016986c3
Omar Ayman <se.omarayman@gmail.com>
2025-05-23 21:05:37 +0300

    fix lint not changing after text changed

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -18,7 +18,7 @@ return {
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
       group = lint_augroup,
       callback = function()
         -- Only run the linter in buffers that you can modify in order to
