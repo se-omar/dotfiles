8d04e26278489f2b340a603ddd9b915049bb8156
Omar Ayman <se.omarayman@gmail.com>
2025-05-24 03:51:35 +0300

    make lazygit floating window full screen

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -33,10 +33,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- commenting
 vim.keymap.set({'v', 'n'}, "<leader>c", ":norm gcc<CR>")
 
