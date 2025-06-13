5a591144c8ccaf93fe410978271f39e632103376
Omar Ayman <se.omarayman@gmail.com>
2025-06-14 00:57:29 +0300

    add symlink file

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+ln -s -f ~/dotfiles/nvim ~/.config
+ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/.xinitrc ~/.xinitrc
+ln -s -f ~/dotfiles/.xprofile ~/.xprofile
+ln -s -f ~/dotfiles/.bashrc ~/.bashrc
+ln -s -f ~/dotfiles/.i3 ~/
+ln -s -f ~/dotfiles/.git ~/.git
+ln -s -f ~/dotfiles/.gitignore ~/.gitignore
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/kitty ~/.config
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/polybar/forest ~/.config/polybar
+ln -s -f ~/dotfiles/rofi ~/.local/share
