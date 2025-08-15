ce0ebfb7541f1c2499afe405adfc617c11f83863
Omar Ayman <se.omarayman@gmail.com>
2025-08-15 17:31:21 +0300

    sync lazy lock

--- nvim/lua/plugins/ts-context-commentstring.lua
+++ nvim/lua/plugins/ts-context-commentstring.lua
@@ -4,10 +4,15 @@ return {
   opts = {
     enable_autocmd = false,
   },
-  config = function()
+  config = function(_, opts)
+    -- This line applies your opts (disabling the autocmd)
+    require('ts_context_commentstring').setup(opts)
+
     local get_option = vim.filetype.get_option
     vim.filetype.get_option = function(filetype, option)
-      return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+      return option == 'commentstring' 
+        and require('ts_context_commentstring.internal').calculate_commentstring() 
+        or get_option(filetype, option)
     end
   end,
 }
